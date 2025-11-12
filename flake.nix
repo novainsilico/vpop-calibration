c4ffa1d0f1ce461e5b4de4843112737958760d1f
Paul Lemarre <paul.lemarre@novainsilico.ai>
2025-11-12 11:30:04 +0100

    feat(env): add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, self, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.outputs.legacyPackages.${system};
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        poetry
+        gcc
+        openssl
+      ];
+      shellHook = ''
+        export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
+      '';
+    };
+  });
+}
