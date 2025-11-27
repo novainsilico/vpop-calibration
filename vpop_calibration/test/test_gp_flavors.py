fix: properly handle NaN values in RMSE computation, set random seed for tests

vpop_calibration/test/test_gp_flavors.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 
 from vpop_calibration import *
+from vpop_calibration.test import *
 
 # Create a dummy training data frame
 patients = ["patient-01", "patient-02"]
@@ -12,17 +13,16 @@ protocol_arms = ["arm-A", "arm-B"]
 time_steps = np.arange(0, 10.0, 1.0)
 patient_descriptors = ["k1", "k2", "k3"]
 gp_params = [*patient_descriptors, "time"]
-rng = np.random.default_rng()
 training_df = pd.DataFrame({"id": patients})
 for descriptor in patient_descriptors:
-    training_df[descriptor] = rng.normal(0, 1, nb_patients)
+    training_df[descriptor] = np_rng.normal(0, 1, nb_patients)
 training_df = training_df.merge(
     pd.DataFrame({"protocol_arm": protocol_arms}), how="cross"
 )
 training_df = training_df.merge(pd.DataFrame({"time": time_steps}), how="cross")
 training_df = training_df.merge(pd.DataFrame({"output_name": obsIds}), how="cross")
-training_df["value"] = rng.normal(0, 1, training_df.shape[0])
-training_df_bootstrapped = training_df.sample(frac=0.9)
+training_df["value"] = np_rng.normal(0, 1, training_df.shape[0])
+training_df_bootstrapped = training_df.sample(frac=0.5, random_state=np_rng)
 
 implemented_kernels = ["RBF", "SMK", "Matern"]
 implemented_var_strat = ["IMV", "LMCV"]
@@ -82,3 +82,4 @@ def test_gp_incomplete_data():
     gp = GP(training_df_bootstrapped, gp_params, nb_training_iter=2)
     gp.train()
     gp.train(mini_batching=True, mini_batch_size=8)
+    gp.eval_perf()

vpop_calibration/test/test_ode_saem.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import pickle
 import uuid
 
 from vpop_calibration import *
+from vpop_calibration.test import *
 
 
 def equations_with_abs(t, y, k_a, k_12, k_21, k_el):
@@ -59,13 +59,12 @@ def test_ode_saem():
     # It should contain at the very minimum one `id` per patient
     nb_patients = 5
     patients_df = pd.DataFrame({"id": [str(uuid.uuid4()) for _ in range(nb_patients)]})
-    rng = np.random.default_rng()
-    patients_df["protocol_arm"] = rng.binomial(1, 0.5, nb_patients)
+    patients_df["protocol_arm"] = np_rng.binomial(1, 0.5, nb_patients)
     patients_df["protocol_arm"] = patients_df["protocol_arm"].apply(
         lambda x: "arm-A" if x == 0 else "arm-B"
     )
-    patients_df["k_a"] = rng.lognormal(-1, 0.1, nb_patients)
-    patients_df["foo"] = rng.lognormal(0.1, 0.1, nb_patients)
+    patients_df["k_a"] = np_rng.lognormal(-1, 0.1, nb_patients)
+    patients_df["foo"] = np_rng.lognormal(0.1, 0.1, nb_patients)
 
     print(f"Simulating {nb_patients} patients on {nb_protocols} protocol arms")
     obs_df = simulate_dataset_from_omega(