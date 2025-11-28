fix: correct scaling in  method of GP class

vpop_calibration/model/data.py
@@ -256,18 +256,6 @@ class TrainingDataSet:
                 rescaled_data[mask] = torch.exp(rescaled_data[mask])
         return rescaled_data
 
-    def normalize_inputs_df(self, inputs_df: pd.DataFrame) -> torch.Tensor:
-        """Normalize new inputs provided to the model as a data frame, and convert them to a tensor."""
-        selected_cols = self.normalizing_input_mean.index.tolist()
-        norm_data = inputs_df[selected_cols]
-        norm_data.loc[:, self.log_inputs] = norm_data.loc[:, self.log_inputs].transform(
-            "log"
-        )
-        norm_data = (
-            norm_data - self.normalizing_input_mean
-        ) / self.normalizing_input_std
-        return torch.Tensor(norm_data.values)
-
     def normalize_inputs_tensor(self, inputs: torch.Tensor) -> torch.Tensor:
         """Normalize new inputs provided to the model as a tensor. The columns of the input tensor should be the same as [self.descriptors]"""
         X = inputs
@@ -378,13 +366,13 @@ class TrainingDataSet:
             new_data["value"] = 1.0
 
         wide_df = self.pivot_input_data(new_data)
-        tensor_inputs_wide = self.normalize_inputs_df(wide_df)
+        tensor_inputs_wide = torch.Tensor(wide_df[self.parameter_names].values)
 
         return tensor_inputs_wide, wide_df, new_data, remove_value
 
     def merge_predictions_long(
         self,
-        pred: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+        pred: tuple[torch.Tensor, torch.Tensor],
         wide_df: pd.DataFrame,
         long_df: pd.DataFrame,
         remove_value: bool,
@@ -398,13 +386,12 @@ class TrainingDataSet:
             remove_value (bool): True if the value column should be ignored
 
         Returns:
-            pd.DataFrame: A merged data frame in a long format, identical to the initial data, with additional columns [`pred_mean`, `pred_low`, `pred_high`]
+            pd.DataFrame: A merged data frame in a long format, identical to the initial data, with additional columns [`pred_mean`, `pred_var`, `pred_low`, `pred_high`]
         """
-        pred_mean, pred_low, pred_high = pred
+        pred_mean, pred_variance = pred
         # Reshape these outputs into a long format
         mean_df = self.pivot_outputs_longer(wide_df, pred_mean, "pred_mean")
-        low_df = self.pivot_outputs_longer(wide_df, pred_low, "pred_low")
-        high_df = self.pivot_outputs_longer(wide_df, pred_high, "pred_high")
+        var_df = self.pivot_outputs_longer(wide_df, pred_variance, "pred_var")
         # Merge the model results with the long format data frame
         out_df = reduce(
             lambda left, right: pd.merge(
@@ -413,7 +400,13 @@ class TrainingDataSet:
                 on=["id"] + self.parameter_names + ["protocol_arm", "output_name"],
                 how="left",
             ),
-            [long_df, mean_df, low_df, high_df],
+            [long_df, mean_df, var_df],
+        )
+        out_df["pred_low"] = out_df.apply(
+            lambda r: r["pred_mean"] - 2 * r["pred_var"], axis=1
+        )
+        out_df["pred_high"] = out_df.apply(
+            lambda r: r["pred_mean"] + 2 * r["pred_var"], axis=1
         )
         # Remove the dummy value column if it was added during the data processing
         if remove_value:

vpop_calibration/nlme.py
@@ -236,7 +236,13 @@ class NlmeModel:
         processed_df["task_index"] = processed_df["task"].apply(
             lambda t: self.structural_model.tasks.index(t)
         )
-
+        global_time_steps = (
+            processed_df["time"].drop_duplicates().sort_values().to_list()
+        )
+        processed_df["time_step_index"] = processed_df["time"].apply(
+            lambda t: global_time_steps.index(t)
+        )
+        self.global_time_steps = torch.Tensor(global_time_steps).unsqueeze(-1)
         self.observations_tensors: Dict = {}
         for patient in self.patients:
             this_patient = processed_df.loc[processed_df["id"] == patient]
@@ -250,17 +256,14 @@ class NlmeModel:
             outputs = torch.Tensor(this_patient["value"].values)
 
             time_steps = torch.Tensor(this_patient["time"].values)
-            unique_time_steps, time_step_indices = torch.unique_consecutive(
-                time_steps, return_inverse=True
-            )
+            time_step_indices = this_patient["time_step_index"].values
 
             self.observations_tensors.update(
                 {
                     patient: {
                         "observations": outputs,
                         "time_steps": time_steps,
-                        "unique_time_steps": unique_time_steps,
-                        "time_step_indices": time_step_indices,
+                        "time_step_indices": torch.LongTensor(time_step_indices),
                         "tasks_indices": torch.LongTensor(tasks_indices),
                         "outputs_indices": torch.LongTensor(outputs_indices),
                     }
@@ -384,19 +387,15 @@ class NlmeModel:
         list_tasks = []
         list_rows = []
         for ind_idx, ind in enumerate(ind_ids):
-            # Prepare the inputs for the GP
-            time_steps = self.observations_tensors[ind]["unique_time_steps"].unsqueeze(
-                -1
-            )
             this_patient_theta_ordered = thetas[ind_idx, self.model_input_to_descriptor]
             thetas_repeated = this_patient_theta_ordered.unsqueeze(0).repeat(
-                (time_steps.shape[0], 1)
+                (self.global_time_steps.shape[0], 1)
             )
             # This steps requires that the `time` is passed as the last input column to the structural model
             inputs = torch.cat(
                 (
                     thetas_repeated,
-                    time_steps,
+                    self.global_time_steps,
                 ),
                 dim=1,
             )
@@ -406,7 +405,7 @@ class NlmeModel:
         return list_X, list_rows, list_tasks
 
     def predict_outputs_from_theta(
-        self, thetas: torch.Tensor, ind_ids: List[str | int]
+        self, thetas: torch.Tensor, ind_ids: Optional[List[str | int]] = None
     ) -> List[torch.Tensor]:
         """Return model predictions for all patients
 
@@ -420,7 +419,9 @@ class NlmeModel:
         list_X, list_rows, list_tasks = self.struc_model_inputs_from_theta(
             thetas, ind_ids
         )
-        pred = self.structural_model.simulate(list_X, list_rows, list_tasks)
+        pred = self.structural_model.simulate(
+            list_X=list_X, list_rows=list_rows, list_tasks=list_tasks
+        )
         return pred
 
     def outputs_to_df(

vpop_calibration/structural_model.py
@@ -77,7 +77,7 @@ class StructuralGp(StructuralModel):
         X_cat = torch.cat(list_X)
         chunk_sizes = [X.shape[0] for X in list_X]
         # Simulate the GP
-        out_cat, _, _ = self.gp_model.predict_wide_scaled(X_cat)
+        out_cat, _ = self.gp_model.predict_wide_scaled(X_cat)
         # Split into individual chunks
         pred_wide_list = torch.split(out_cat, chunk_sizes)
         pred_list = []
@@ -157,7 +157,7 @@ class StructuralOdeModel(StructuralModel):
         for X, rows, tasks in zip(list_X, list_rows, list_tasks):
             temp_id = str(uuid.uuid4())
             # store the size of X for proper splitting
-            chunks_list.append(X.shape[0])
+            chunks_list.append(rows.shape[0])
             # Extract the parameters and time values
             params = X.index_select(0, rows).detach().numpy()
             # Extract the task order
@@ -167,6 +167,9 @@ class StructuralOdeModel(StructuralModel):
             input_df_temp = pd.DataFrame(
                 data=params, columns=self.parameter_names + ["time"]
             )
+            # The passed params include the _global_ time steps
+            # Filter the time steps that we actually want for this patient
+            input_df_temp = input_df_temp.iloc[rows.numpy()]
             # Add the task index as a temporary column
             input_df_temp["task_index"] = task_index
             # Deduce protocol arm and output name from task index