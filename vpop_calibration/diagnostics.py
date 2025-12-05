feat: add posterior validation with training range, add pred variance threshold flag during iterations, correct implementation for data generation (include residual error)

vpop_calibration/diagnostics.py
@@ -6,10 +6,10 @@ from .nlme import NlmeModel
 from .saem import PySaem
 from .model.gp import GP
 from .structural_model import StructuralGp
+from .utils import smoke_test
 
 
-def check_surrogate_validity_gp(optimizer: PySaem):
-    nlme_model: NlmeModel = optimizer.model
+def check_surrogate_validity_gp(nlme_model: NlmeModel) -> tuple[dict, dict]:
     pdus = nlme_model.descriptors
     gp_model_struct = nlme_model.structural_model
     assert isinstance(
@@ -19,8 +19,7 @@ def check_surrogate_validity_gp(optimizer: PySaem):
     gp_model: GP = gp_model_struct.gp_model
     train_data = gp_model.data.full_df_raw[pdus].drop_duplicates()
 
-    map_estimates = optimizer.current_thetas
-    map_data = pd.DataFrame(data=map_estimates.numpy(), columns=pdus)
+    map_data = nlme_model.map_estimates_descriptors()
     patients = nlme_model.patients
 
     n_plots = len(pdus)
@@ -99,3 +98,65 @@ def check_surrogate_validity_gp(optimizer: PySaem):
             if k1 == len(pdus) - 1:
                 ax.set_xlabel(param2)
     return diagnostics, recommended_ranges
+
+
+def plot_map_estimates(nlme_model: NlmeModel) -> None:
+    observed = nlme_model.observations_df
+    simulated_df = nlme_model.map_estimates_predictions()
+
+    n_cols = nlme_model.nb_outputs
+    n_rows = nlme_model.structural_model.nb_protocols
+    _, axes = plt.subplots(
+        n_rows, n_cols, figsize=(5 * n_cols, 4 * n_rows), squeeze=False
+    )
+
+    cmap = plt.get_cmap("Spectral")
+    colors = cmap(np.linspace(0, 1, nlme_model.nb_patients))
+    for output_index, output_name in enumerate(nlme_model.outputs_names):
+        for protocol_index, protocol_arm in enumerate(
+            nlme_model.structural_model.protocols
+        ):
+            obs_loop = observed.loc[
+                (observed["output_name"] == output_name)
+                & (observed["protocol_arm"] == protocol_arm)
+            ]
+            pred_loop = simulated_df.loc[
+                (simulated_df["output_name"] == output_name)
+                & (simulated_df["protocol_arm"] == protocol_arm)
+            ]
+            ax = axes[protocol_index, output_index]
+            ax.set_xlabel("Time")
+            patients_protocol = obs_loop["id"].drop_duplicates().to_list()
+            for patient_ind in patients_protocol:
+                patient_num = nlme_model.patients.index(patient_ind)
+                patient_obs = obs_loop.loc[obs_loop["id"] == patient_ind]
+                patient_pred = pred_loop.loc[pred_loop["id"] == patient_ind]
+                time_vec = patient_obs["time"].values
+                sorted_indices = np.argsort(time_vec)
+                sorted_times = time_vec[sorted_indices]
+                obs_vec = patient_obs["value"].values[sorted_indices]
+                ax.plot(
+                    sorted_times,
+                    obs_vec,
+                    "+",
+                    color=colors[patient_num],
+                    linewidth=2,
+                    alpha=0.6,
+                )
+                if patient_pred.shape[0] > 0:
+                    pred_vec = patient_pred["predicted_value"].values[sorted_indices]
+                    ax.plot(
+                        sorted_times,
+                        pred_vec,
+                        "-",
+                        color=colors[patient_num],
+                        linewidth=2,
+                        alpha=0.5,
+                    )
+
+            title = f"{output_name} in {protocol_arm}"  # More descriptive title
+            ax.set_title(title)
+
+    if not smoke_test:
+        plt.tight_layout()
+        plt.show()

vpop_calibration/nlme.py
@@ -16,6 +16,7 @@ class NlmeModel:
         init_res_var: list[float],
         covariate_map: Optional[dict[str, dict[str, dict[str, str | float]]]] = None,
         error_model_type: str = "additive",
+        pred_var_threshold: float = 1e-2,
     ):
         """Create a non-linear mixed effects model
 
@@ -35,8 +36,10 @@ class NlmeModel:
                     }
                 }
             error_model_type (str): either `additive` or `proportional` error model
+            pred_var_threshold (float): Threshold of predictive variance that will issue a warning. Default 1e-2.
         """
         self.structural_model: StructuralModel = structural_model
+        self.pred_var_threshold = pred_var_threshold
 
         self.MI_names: list[str] = list(init_log_MI.keys())
         self.nb_MI: int = len(self.MI_names)
@@ -147,7 +150,7 @@ class NlmeModel:
         self.error_model_type: str = error_model_type
         self.init_res_var = torch.Tensor(init_res_var)
         self.init_omega = torch.diag(
-            torch.tensor([float(pdu["sd"] ** 2) for pdu in init_PDU.values()])
+            torch.tensor([float(init_PDU[pdu]["sd"]) for pdu in self.PDU_names])
         )
 
         # Assemble the list of design matrices from the covariance structure
@@ -162,6 +165,10 @@ class NlmeModel:
         self.eta_distribution = torch.distributions.MultivariateNormal(
             loc=torch.zeros(self.nb_PDU),
             covariance_matrix=self.omega_pop,
+        )
+        self.current_eta_samples = self.sample_individual_etas()
+        self.current_map_estimates = self.individual_parameters(
+            self.current_eta_samples
         )
 
     def _create_design_matrix(self, covariates: dict[str, float]) -> torch.Tensor:
@@ -207,6 +214,8 @@ class NlmeModel:
             - `output_name`
             - `value`
         """
+        # Store the raw data frame
+        self.observations_df = observations_df
         # Data validation
         input_columns = observations_df.columns.tolist()
         unique_outputs = observations_df["output_name"].unique().tolist()
@@ -253,14 +262,22 @@ class NlmeModel:
         )
         self.global_time_steps = torch.Tensor(global_time_steps).unsqueeze(-1)
         self.observations_tensors: dict = {}
+        self.n_tot_observations = torch.zeros(self.nb_outputs)
         for patient in self.patients:
             this_patient = processed_df.loc[processed_df["id"] == patient]
 
             tasks_indices = this_patient["task_index"].values
-            outputs_indices = [
-                self.structural_model.task_idx_to_output_idx[task]
-                for task in tasks_indices
-            ]
+            outputs_indices = torch.LongTensor(
+                [
+                    self.structural_model.task_idx_to_output_idx[task]
+                    for task in tasks_indices
+                ]
+            )
+            self.n_tot_observations.scatter_add_(
+                0,
+                outputs_indices,
+                torch.ones_like(outputs_indices, dtype=torch.float64),
+            )
 
             outputs = torch.Tensor(this_patient["value"].values)
 
@@ -281,7 +298,9 @@ class NlmeModel:
 
     def update_omega(self, omega: torch.Tensor) -> None:
         """Update the covariance matrix of the NLME model."""
-        assert self.omega_pop.shape == omega.shape, "Wrong omega shape"
+        assert (
+            self.omega_pop.shape == omega.shape
+        ), f"Wrong omega shape: {omega.shape}, expected: {self.omega_pop.shape}"
         self.omega_pop = omega
         self.omega_pop_lower_chol = torch.linalg.cholesky(self.omega_pop)
         self.eta_distribution = torch.distributions.MultivariateNormal(
@@ -291,18 +310,38 @@ class NlmeModel:
 
     def update_res_var(self, residual_var: torch.Tensor) -> None:
         """Update the residual variance of the NLME model."""
-        assert self.residual_var.shape == residual_var.shape, "Wrong res var shape"
+        assert (
+            self.residual_var.shape == residual_var.shape
+        ), f"Wrong res var shape: {residual_var.shape}, expected: {self.residual_var.shape}"
         self.residual_var = residual_var
 
     def update_betas(self, betas: torch.Tensor) -> None:
         """Update the betas of the NLME model."""
-        assert self.population_betas.shape == betas.shape, "Wrong beta shape"
+        assert (
+            self.population_betas.shape == betas.shape
+        ), f"Wrong beta shape: {betas.shape}, expected: {self.population_betas.shape}"
         self.population_betas = betas
 
     def update_log_mi(self, log_MI: torch.Tensor) -> None:
         """Update the model intrinsic parameter values of the NLME model."""
-        assert self.log_MI.shape == log_MI.shape, "Wrong MI shape"
+        assert (
+            self.log_MI.shape == log_MI.shape
+        ), f"Wrong MI shape: {log_MI.shape}, expected: {self.log_MI.shape}"
         self.log_MI = log_MI
+
+    def update_eta_samples(self, eta: torch.Tensor) -> None:
+        """Update the model current individual random effect sampels."""
+        assert (
+            self.current_eta_samples.shape == eta.shape
+        ), f"Wrong individual samples shape: {eta.shape}, expected: {self.current_eta_samples.shape}"
+        self.current_eta_samples = eta
+
+    def update_map_estimates(self, theta: torch.Tensor) -> None:
+        """Update the model current maximum a posteriori estimates."""
+        assert (
+            self.current_map_estimates.shape == theta.shape
+        ), f"Wrong individual parameters shape: {theta.shape}, expected: {self.current_map_estimates.shape}"
+        self.current_map_estimates = theta
 
     def sample_individual_etas(self, nb_patients=None) -> torch.Tensor:
         """Sample individual random effects from the current estimate of Omega
@@ -433,8 +472,28 @@ class NlmeModel:
         )
         return pred_mean, pred_var
 
+    def add_residual_error(
+        self, outputs: list[torch.Tensor], ind_ids: Optional[list[str | int]] = None
+    ) -> list[torch.Tensor]:
+        if ind_ids is None:
+            ind_ids = self.patients
+        transformed_out = []
+        for ind_idx, ind in enumerate(ind_ids):
+            output_indx = self.observations_tensors[ind]["outputs_indices"]
+            n_obs = output_indx.shape[0]
+            res_var = self.residual_var.index_select(0, output_indx)
+            noise = torch.distributions.Normal(torch.zeros(n_obs), res_var).sample()
+            if self.error_model_type == "additive":
+                new_out = outputs[ind_idx] + noise
+            elif self.error_model_type == "proportional":
+                new_out = outputs[ind_idx] * noise
+            else:
+                raise ValueError(f"Non-implemented error model {self.error_model_type}")
+            transformed_out.append(new_out)
+        return transformed_out
+
     def outputs_to_df(
-        self, outputs: list[torch.Tensor], ind_ids: list[str | int]
+        self, outputs: list[torch.Tensor], ind_ids: Optional[list[str | int]] = None
     ) -> pd.DataFrame:
         """Transform the NLME model outputs to a data frame in order to compare with observed data
 
@@ -450,6 +509,8 @@ class NlmeModel:
             - `time`
             - `predicted_value`
         """
+        if ind_ids is None:
+            ind_ids = self.patients
         df_list = []
         for ind_idx, ind in enumerate(ind_ids):
             time_steps = self.observations_tensors[ind]["time_steps"]
@@ -496,7 +557,13 @@ class NlmeModel:
         self,
         etas: torch.Tensor,
         ind_ids_for_etas: Optional[list[Union[str, int]]] = None,
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, dict[int | str, torch.Tensor]]:
+    ) -> tuple[
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        dict[int | str, torch.Tensor],
+        list,
+    ]:
         """Compute the log-posterior of a list of random effects
 
         Args:
@@ -539,6 +606,10 @@ class NlmeModel:
             individual_params, ind_ids_for_etas
         )
 
+        # Validate the variance magnitude
+        warnings = self.variance_level_check(full_var, self.pred_var_threshold)
+        flagged_patients = [individual_params[i, :].tolist() for i in warnings]
+
         # calculate log-prior of the random samples
         log_priors: torch.Tensor = self._log_prior_etas(etas)
 
@@ -555,7 +626,13 @@ class NlmeModel:
         current_log_pdu = torch.log(
             individual_params[:, self.nb_PDK : self.nb_PDK + self.nb_PDU]
         )
-        return log_posterior, individual_params, current_log_pdu, pred_dict
+        return (
+            log_posterior,
+            individual_params,
+            current_log_pdu,
+            pred_dict,
+            flagged_patients,
+        )
 
     def calculate_residuals(
         self, observed_data: torch.Tensor, predictions: torch.Tensor
@@ -578,8 +655,8 @@ class NlmeModel:
 
     def sum_sq_residuals(self, pred: dict[str | int, torch.Tensor]) -> torch.Tensor:
         sum_residuals = torch.zeros(self.nb_outputs)
-        for output_ind in range(self.nb_outputs):
-            for patient in self.patients:
+        for patient in self.patients:
+            for output_ind in range(self.nb_outputs):
                 mask = torch.BoolTensor(
                     self.observations_tensors[patient]["outputs_indices"] == output_ind
                 )
@@ -587,13 +664,10 @@ class NlmeModel:
                 if n_obs > 0:
                     observed = self.observations_tensors[patient]["observations"][mask]
                     predicted = pred[patient][mask]
-                    sum_residuals[output_ind] += (
-                        torch.square(
-                            self.calculate_residuals(observed, predicted)
-                        ).sum()
-                        / n_obs
-                    )
-        return sum_residuals / self.nb_patients
+                    sum_residuals[output_ind] += torch.square(
+                        self.calculate_residuals(observed, predicted)
+                    ).sum()
+        return sum_residuals
 
     def log_likelihood_observation(
         self,
@@ -642,10 +716,12 @@ class NlmeModel:
     ) -> tuple[
         torch.Tensor,
         torch.Tensor,
+        torch.Tensor,
         dict[str | int, torch.Tensor],
         torch.Tensor,
         torch.Tensor,
         float,
+        list,
     ]:
         """Perform one step of a Metropolis-Hastings transition kernel
 
@@ -667,13 +743,18 @@ class NlmeModel:
             - updated thetas
             - updated values of log PDUs
             - updated step size
+            - a dict of warnings for all patients with predictive variance above threshold
         """
 
         proposal_noise = torch.randn_like(current_etas) @ self.omega_pop_lower_chol
         proposal_etas = current_etas + step_size * proposal_noise
-        proposal_log_prob, proposal_theta, proposal_log_pdus, proposal_pred = (
-            self.log_posterior_etas(proposal_etas)
-        )
+        (
+            proposal_log_prob,
+            proposal_theta,
+            proposal_log_pdus,
+            proposal_pred,
+            warnings,
+        ) = self.log_posterior_etas(proposal_etas)
         deltas: torch.Tensor = proposal_log_prob - current_log_prob
         log_u: torch.Tensor = torch.log(torch.rand_like(deltas))
         accept_mask: torch.Tensor = log_u < deltas
@@ -684,14 +765,15 @@ class NlmeModel:
         new_etas = torch.where(accept_mask_extended, proposal_etas, current_etas)
         new_log_pdus = torch.where(accept_mask_extended, proposal_log_pdus, current_pdu)
         new_log_prob = torch.where(accept_mask, proposal_log_prob, current_log_prob)
+        new_complete_likelihood = -2 * new_log_prob.sum(dim=0)
         new_pred = {
             patient: proposal_pred[patient] if accept_mask[i] else current_pred[patient]
             for i, patient in enumerate(self.patients)
         }
         new_acceptance_rate: float = accept_mask.float().mean().item()
         if verbose:
-            print(f"Acceptance rate: {new_acceptance_rate}")
-        new_step_size: float = np.exp(
+            print(f"  Acceptance rate: {new_acceptance_rate:.2f}")
+        new_step_size: float = step_size * np.exp(
             learning_rate * (new_acceptance_rate - target_acceptance_rate)
         )
         new_theta = torch.where(
@@ -702,8 +784,38 @@ class NlmeModel:
         return (
             new_etas,
             new_log_prob,
+            new_complete_likelihood,
             new_pred,
             new_theta,
             new_log_pdus,
             new_step_size,
+            warnings,
         )
+
+    def map_estimates_descriptors(self) -> pd.DataFrame:
+        theta = self.current_map_estimates
+        if theta is None:
+            raise ValueError("No estimation available yet. Run the algorithm first.")
+
+        map_per_patient = pd.DataFrame(data=theta.numpy(), columns=self.descriptors)
+        return map_per_patient
+
+    def map_estimates_predictions(self) -> pd.DataFrame:
+        theta = self.current_map_estimates
+        if theta is None:
+            raise ValueError(
+                "No estimation available yet. Run the optimization algorithm first."
+            )
+        simulated_tensor, _ = self.predict_outputs_from_theta(theta, self.patients)
+        simulated_df = self.outputs_to_df(simulated_tensor, self.patients)
+        return simulated_df
+
+    def variance_level_check(
+        self, var_list: list[torch.Tensor], threshold: float
+    ) -> list:
+        warnings = []
+        for i in range(self.nb_patients):
+            var, _ = var_list[i].max(0)
+            if var > threshold:
+                warnings.append(i)
+        return warnings

vpop_calibration/saem.py
@@ -64,7 +64,6 @@ class PySaem:
 
         self.model: NlmeModel = model
         self.model.add_observations(observations_df)
-        self.observations_df = observations_df
         # MCMC sampling in the E-step parameters
         self.mcmc_first_burn_in: int = mcmc_first_burn_in
         self.mcmc_nb_transitions: int = mcmc_nb_transitions
@@ -115,9 +114,7 @@ class PySaem:
         self.plot_indiv_figsize = plot_indiv_figsize
 
         # Initialize the random effects to 0
-        self.current_etas: torch.Tensor = torch.zeros(
-            (self.model.nb_patients, self.model.nb_PDU)
-        )
+        self.current_etas = self.model.current_eta_samples
 
         # Initialize current estimation of patient parameters from the 0 random effects
         (
@@ -125,7 +122,9 @@ class PySaem:
             self.current_thetas,
             self.current_log_pdu,
             self.current_pred,
+            flagged_patients,
         ) = self.model.log_posterior_etas(self.current_etas)
+        self.current_complete_likelihood = torch.exp(self.current_log_prob.sum(dim=0))
 
         # Initialize the optimizer history
         self._init_history(
@@ -133,6 +132,8 @@ class PySaem:
             self.model.omega_pop,
             self.model.log_MI,
             self.model.residual_var,
+            self.current_complete_likelihood,
+            flagged_patients,
         )
         self.current_iteration: int = 0
 
@@ -339,6 +340,8 @@ class PySaem:
         omega: torch.Tensor,
         log_mi: torch.Tensor,
         res_var: torch.Tensor,
+        likelihood: torch.Tensor,
+        flagged_patients: list,
     ) -> None:
         # Initialize the history
         self.history = {}
@@ -365,6 +368,8 @@ class PySaem:
         # Add the residual variance
         for i, output in enumerate(self.model.outputs_names):
             self.history.update({output: [res_var[i]]})
+        self.history.update({"complete_likelihood": [likelihood]})
+        self.history.update({"flagged_patients": [flagged_patients]})
 
     def _append_history(
         self,
@@ -372,6 +377,8 @@ class PySaem:
         omega: torch.Tensor,
         log_mi: torch.Tensor,
         res_var: torch.Tensor,
+        complete_likelihood: torch.Tensor,
+        flagged_patients: list,
     ) -> None:
         # Update the history
         for i, pdu in enumerate(self.model.PDU_names):
@@ -390,6 +397,8 @@ class PySaem:
 
         for i, output in enumerate(self.model.outputs_names):
             self.history[output].append(res_var[i])
+        self.history["complete_likelihood"].append(complete_likelihood)
+        self.history["flagged_patients"].append(flagged_patients)
 
     def one_iteration(self, k: int) -> bool:
         """Perform one iteration of SAEM
@@ -400,7 +409,6 @@ class PySaem:
 
         if self.verbose:
             print(f"Running iteration {k}")
-            print(self.current_thetas.shape)
         # If first iteration, consider burn in
         if k == 0:
             current_iter_burn_in = self.mcmc_first_burn_in
@@ -413,19 +421,22 @@ class PySaem:
 
         # --- E-step: perform MCMC kernel transitions
         if self.verbose:
-            print(f"Current learning rate: {self.learning_rate_m_step: .2f}")
             print("  MCMC sampling")
+            print(
+                f"  Current MCMC parameters: step-size={self.step_size:.2f}, adaptation rate={self.step_size_adaptation:.2f}"
+            )
 
+        flagged_patients_iter = []
         for _ in range(current_iter_burn_in + self.mcmc_nb_transitions):
-            if self.verbose:
-                print(f"  Current MCMC step-size: {self.step_size: .2f}")
             (
                 self.current_etas,
                 self.current_log_prob,
+                self.current_complete_likelihood,
                 self.current_pred,
                 self.current_thetas,
                 self.current_log_pdu,
                 self.step_size,
+                flagged_patients,
             ) = self.model.mh_step(
                 current_etas=self.current_etas,
                 current_log_prob=self.current_log_prob,
@@ -436,15 +447,17 @@ class PySaem:
                 learning_rate=self.step_size_adaptation,
                 verbose=self.verbose,
             )
+            flagged_patients_iter += flagged_patients
+
+        # Update the model's eta and thetas
+        self.model.update_eta_samples(self.current_etas)
+        self.model.update_map_estimates(self.current_thetas)
 
         # --- M-Step: Update Population Means, Omega and Residual variance ---
 
         # 1. Update residual error variances
-
-        if self.verbose:
-            print("  Res var update")
-
-        target_res_var: torch.Tensor = self.model.sum_sq_residuals(self.current_pred)
+        sum_sq_res = self.model.sum_sq_residuals(self.current_pred)
+        target_res_var: torch.Tensor = sum_sq_res / self.model.n_tot_observations
         current_res_var: torch.Tensor = self.model.residual_var
         if k < self.nb_phase1_iterations:
             target_res_var = self._simulated_annealing(current_res_var, target_res_var)
@@ -456,8 +469,6 @@ class PySaem:
         self.model.update_res_var(new_residual_error_var)
 
         # 2. Update sufficient statistics with stochastic approximation
-        if self.verbose:
-            print("  M-step update:")
         (
             self.sufficient_stat_cross_product,
             self.sufficient_stat_outer_product,
@@ -468,11 +479,13 @@ class PySaem:
             self.sufficient_stat_cross_product,
             self.sufficient_stat_outer_product,
         )
+
         # Update beta
         self.model.update_betas(new_beta)
 
-        # Update omega with simulated annealing (phase 1)
+        # Update omega
         if k < self.nb_phase1_iterations:
+            # Simulated annealing during phase 1
             new_omega_diag = torch.diag(new_omega)
             current_omega_diag = torch.diag(self.model.omega_pop)
             annealed_omega_diag = self._simulated_annealing(
@@ -526,6 +539,8 @@ class PySaem:
             self.model.omega_pop,
             self.model.log_MI,
             self.model.residual_var,
+            self.current_complete_likelihood,
+            flagged_patients_iter,
         )
 
         # update prev_params for the next iteration's convergence check
@@ -712,7 +727,7 @@ class PySaem:
         nb_betas: int = self.model.nb_betas
         nb_omega_diag_params: int = self.model.nb_PDU
         nb_var_res_params: int = self.model.nb_outputs
-        nb_plots = nb_MI + nb_betas + nb_omega_diag_params + nb_var_res_params
+        nb_plots = nb_MI + nb_betas + nb_omega_diag_params + nb_var_res_params + 2
         nb_cols = n_cols
         nb_rows = int(np.ceil(nb_plots / nb_cols))
         maxiter = self.nb_phase1_iterations + self.nb_phase2_iterations
@@ -843,6 +858,32 @@ class PySaem:
             ax.grid(True)
             self.traces.update({res_name: tr})
             plot_idx += 1
+        # Plot the convergence indicator (total log prob)
+        row, col = plot_idx // nb_cols, plot_idx % nb_cols
+        ax = axes[row, col]
+        ax.set_xlim(0, maxiter)
+        convergence_ind = [h.item() for h in history["complete_likelihood"]]
+        (tr,) = ax.plot(
+            convergence_ind,
+        )
+        ax.set_title(rf"Convergence indicator")
+        ax.grid(True)
+        self.traces.update({"convergence_ind": tr})
+        plot_idx += 1
+        # Plot the number of out of bounds patients
+        row, col = plot_idx // nb_cols, plot_idx % nb_cols
+        ax = axes[row, col]
+        ax.set_xlim(0, maxiter)
+        ax.set_ylim(0, self.model.nb_patients)
+        oob_patients = [len(h) for h in history["flagged_patients"]]
+        (tr,) = ax.plot(
+            oob_patients,
+        )
+        ax.set_title(rf"Out-of-bounds patients")
+        ax.grid(True)
+        self.traces.update({"oob_patients": tr})
+        plot_idx += 1
+
         # Turn off extra subplots
         while plot_idx < nb_rows * nb_cols:
             row, col = plot_idx // nb_cols, plot_idx % nb_cols
@@ -879,12 +920,16 @@ class PySaem:
         for res_name in self.model.outputs_names:
             var_res_history = [h.item() for h in history[res_name]]
             self.traces[res_name].set_data(new_xaxis, var_res_history)
-
-        for ax in self.convergence_plot_axes.flatten():
-            ax.autoscale_view(scaley=True, scalex=False)
-            ax.relim()
-        if self.convergence_plot_handle is not None:
-            self.convergence_plot_handle.update(self.convergence_plot_fig)
+        conv_ind = [h.item() for h in history["complete_likelihood"]]
+        self.traces["convergence_ind"].set_data(new_xaxis, conv_ind)
+        oob_patients = [len(h) for h in history["flagged_patients"]]
+        self.traces["oob_patients"].set_data(new_xaxis, oob_patients)
+        if not smoke_test:
+            for ax in self.convergence_plot_axes.flatten():
+                ax.autoscale_view(scaley=True, scalex=False)
+                ax.relim()
+            if self.convergence_plot_handle is not None:
+                self.convergence_plot_handle.update(self.convergence_plot_fig)
 
     def plot_convergence_history(
         self,
@@ -895,88 +940,3 @@ class PySaem:
             indiv_figsize=indiv_figsize, n_cols=n_cols
         )
         plt.close(fig)
-
-    def map_estimates_descriptors(self) -> pd.DataFrame:
-        theta = self.current_thetas
-        if theta is None:
-            raise ValueError("No estimation available yet. Run the algorithm first.")
-
-        map_per_patient = pd.DataFrame(
-            data=theta.numpy(), columns=self.model.descriptors
-        )
-        return map_per_patient
-
-    def map_estimates_predictions(self) -> pd.DataFrame:
-        theta = self.current_thetas
-        if theta is None:
-            raise ValueError(
-                "No estimation available yet. Run the optimization algorithm first."
-            )
-        simulated_tensor, _ = self.model.predict_outputs_from_theta(
-            theta, self.model.patients
-        )
-        simulated_df = self.model.outputs_to_df(simulated_tensor, self.model.patients)
-        return simulated_df
-
-    def plot_map_estimates(self) -> None:
-        observed = self.observations_df
-        simulated_df = self.map_estimates_predictions()
-
-        n_cols = self.model.nb_outputs
-        n_rows = self.model.structural_model.nb_protocols
-        _, axes = plt.subplots(
-            n_rows, n_cols, figsize=(5 * n_cols, 4 * n_rows), squeeze=False
-        )
-
-        cmap = plt.get_cmap("Spectral")
-        colors = cmap(np.linspace(0, 1, self.model.nb_patients))
-        for output_index, output_name in enumerate(self.model.outputs_names):
-            for protocol_index, protocol_arm in enumerate(
-                self.model.structural_model.protocols
-            ):
-                obs_loop = observed.loc[
-                    (observed["output_name"] == output_name)
-                    & (observed["protocol_arm"] == protocol_arm)
-                ]
-                pred_loop = simulated_df.loc[
-                    (simulated_df["output_name"] == output_name)
-                    & (simulated_df["protocol_arm"] == protocol_arm)
-                ]
-                ax = axes[protocol_index, output_index]
-                ax.set_xlabel("Time")
-                patients_protocol = obs_loop["id"].drop_duplicates().to_list()
-                for patient_ind in patients_protocol:
-                    patient_num = self.model.patients.index(patient_ind)
-                    patient_obs = obs_loop.loc[obs_loop["id"] == patient_ind]
-                    patient_pred = pred_loop.loc[pred_loop["id"] == patient_ind]
-                    time_vec = patient_obs["time"].values
-                    sorted_indices = np.argsort(time_vec)
-                    sorted_times = time_vec[sorted_indices]
-                    obs_vec = patient_obs["value"].values[sorted_indices]
-                    ax.plot(
-                        sorted_times,
-                        obs_vec,
-                        "+",
-                        color=colors[patient_num],
-                        linewidth=2,
-                        alpha=0.6,
-                    )
-                    if patient_pred.shape[0] > 0:
-                        pred_vec = patient_pred["predicted_value"].values[
-                            sorted_indices
-                        ]
-                        ax.plot(
-                            sorted_times,
-                            pred_vec,
-                            "-",
-                            color=colors[patient_num],
-                            linewidth=2,
-                            alpha=0.5,
-                        )
-
-                title = f"{output_name} in {protocol_arm}"  # More descriptive title
-                ax.set_title(title)
-
-        if not smoke_test:
-            plt.tight_layout()
-            plt.show()

vpop_calibration/structural_model.py
@@ -200,7 +200,8 @@ class StructuralOdeModel(StructuralModel):
         output_df = self.ode_model.simulate_model(full_input)
         # Convert back to tensor
         out_tensor = torch.Tensor(output_df["predicted_value"].values)
+        out_var = torch.zeros_like(out_tensor)
         # Split into chunks
         out_list = list(torch.split(out_tensor, chunks_list))
-        var_list = []
+        var_list = list(torch.split(out_var, chunks_list))
         return out_list, var_list