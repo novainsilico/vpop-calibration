ef638307b3378ab9d049d4c046d2a6617c04a286
Eliott Tixier <eliott.tixier@novainsilico.ai>
2026-03-12 17:23:14 +0100

    chore: add support for protocols in analytical structural model

--- vpop_calibration/structural_model.py
+++ vpop_calibration/structural_model.py
@@ -2,7 +2,8 @@ import torch
 import pandas as pd
 import numpy as np
 import uuid
-from typing import Callable
+from typing import Callable, Optional
+import itertools
 
 from .model.gp import GP
 from .ode import OdeModel
@@ -92,7 +93,6 @@ class StructuralGp(StructuralModel):
         # Simulate the GP
         X_vertical = X.view(-1, nb_params)
         out_cat, var_cat = self.gp_model.predict_wide_scaled(X_vertical)
-        num_tasks = out_cat.shape[-1]
         out_wide = out_cat.view(num_chains, nb_patients, nb_timesteps, -1)
         var_wide = var_cat.view(num_chains, nb_patients, nb_timesteps, -1)
 
@@ -165,8 +165,7 @@ class StructuralOdeModel(StructuralModel):
         prediction_index: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
         chunks: list[int],
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        (nb_chains, nb_patients, nb_timesteps, nb_params) = X.shape
-        patient_index_full, rows_full, tasks_full = prediction_index
+        _patient_index_full, rows_full, tasks_full = prediction_index
         list_rows = torch.split(rows_full, chunks)
         list_tasks = torch.split(tasks_full, chunks)
 
@@ -226,38 +225,89 @@ class StructuralOdeModel(StructuralModel):
 
 class StructuralAnalytical(StructuralModel):
     def __init__(
-        self, equations: Callable, parameter_names: list[str], variable_names: list[str]
+        self,
+        equations: Callable,
+        variable_names: list[str],
+        protocol_design: Optional[pd.DataFrame] = None,
     ):
+        # Extracts the ORDERED list of arguments from the "equations" function signature
+        # This will be used to map the columns of the input tensor to their corresponding positional arguments
+        # when invoking "equations"
+        function_arguments = list(
+            # 1. ".__code__.co_varnames" is a tuple of ALL the variables (including local variables) referenced in
+            # the function, STARTING with the function arguments which is the only reason this works.
+            # 2. # ".__code__.co_argcount" is the number of function arguments
+            # See the "inspect" doc for more details
+            # https://docs.python.org/3/library/inspect.html
+            equations.__code__.co_varnames[: equations.__code__.co_argcount]
+        )
         self.equations = equations
-        output_names: list[str] = variable_names
-        protocol_arms = ["identity"]
-        tasks: list[str] = [
-            output + "_" + protocol
-            for protocol in protocol_arms
-            for output in output_names
+        if protocol_design is None:
+            protocol_design = pd.DataFrame({"protocol_arm": ["identity"]})
+        protocol_arms = protocol_design["protocol_arm"].drop_duplicates().to_list()
+        protocol_arm_idx = {
+            arm: arm_index for arm_index, arm in enumerate(protocol_arms)
+        }
+        variable_idx = {
+            variable_name: variable_index
+            for variable_index, variable_name in enumerate(variable_names)
+        }
+        protocol_overrides_set = set(protocol_design.drop(columns="protocol_arm"))
+        # the parameters of the "equations" function which are NOT protocol overrides and NOT time, in this order
+        parameter_names_without_protocol_overrides = [
+            p
+            for p in function_arguments
+            if p not in protocol_overrides_set and p != "t"
         ]
-        # Map tasks to output names
-        task_to_output = {
-            output_name + "_" + protocol_arm: output_name
-            for output_name in output_names
-            for protocol_arm in protocol_arms
-        }
-        # Map task index to output index
-        task_idx_to_output_idx = {
-            tasks.index(k): output_names.index(v) for k, v in task_to_output.items()
-        }
-        # Map task to protocol arm
-        task_to_protocol = {
-            output_name + "_" + protocol_arm: protocol_arm
-            for output_name in output_names
-            for protocol_arm in protocol_arms
-        }
-        # Map task index to protocol arm
-        task_idx_to_protocol = {tasks.index(k): v for k, v in task_to_protocol.items()}
+        # the parameters of the "equations" function which are protocol overrides, in this order
+        protocol_parameters = [
+            p for p in function_arguments if p in protocol_overrides_set
+        ]
+        self.nb_protocol_overrides = len(protocol_parameters)
+
+        # time is special cases in "nlme.py" and always comes after the structural parameters
+        input_parameters = (
+            parameter_names_without_protocol_overrides + ["t"] + protocol_parameters
+        )
+        parameter_index = {p: index for (index, p) in enumerate(input_parameters)}
+        self.input_tensor_column_index_to_function_parameter_index = [
+            parameter_index[a] for a in function_arguments
+        ]
+
+        # The tensor of protocol overrides with shape nb_arms x nb_protocol_params
+        # np.atleast_2d() ensures the array has at least shape (1, 0) if there are not protocol overrides
+        # (e.g. in the "identity" protocol)
+        if len(protocol_parameters) == 0:
+            protocol_tensor = np.zeros((len(protocol_arms), 0))
+        else:
+            protocol_tensor = np.atleast_2d(
+                np.transpose(
+                    np.array([protocol_design[p] for p in protocol_parameters])
+                )
+            )
+        # Building the tasks names and the various index maps in one go
+        tasks = []
+        task_idx_to_output_idx = {}
+        task_idx_to_protocol = {}
+        task_protocol_overrides = []
+        for task_index, (arm, output) in enumerate(
+            itertools.product(protocol_arms, variable_names)
+        ):
+            task_name = f"{output}_{arm}"
+            tasks.append(task_name)
+            task_idx_to_output_idx[task_index] = variable_idx[output]
+            task_idx_to_protocol[task_index] = arm
+            arm_idx = protocol_arm_idx[arm]
+            task_protocol_overrides.append(np.atleast_1d(protocol_tensor[arm_idx, :]))
+        # the protocol overrides are stored in tensor of shape (num_tasks, nb_protocol_params)
+        # such that it can later be efficiently appended to the input tensor "X" in simulate()
+        self.task_protocol_tensor = torch.as_tensor(
+            np.array(task_protocol_overrides), device=device
+        )
 
         super().__init__(
-            parameter_names,
-            output_names,
+            parameter_names_without_protocol_overrides,
+            variable_names,
             protocol_arms,
             tasks,
             task_idx_to_output_idx,
@@ -271,16 +321,27 @@ class StructuralAnalytical(StructuralModel):
         chunks: list[int],
     ) -> tuple[torch.Tensor, torch.Tensor]:
         (num_chains, nb_patients, nb_timesteps, nb_params) = X.shape
-        nb_obs_per_chain = prediction_index[0].shape[0]
+        patient_index, timestep_index, task_index = prediction_index
+        protocol_overrides = self.task_protocol_tensor[task_index].reshape(
+            num_chains, nb_patients, nb_timesteps, self.nb_protocol_overrides
+        )
+        X_with_protocol_overrides = torch.concat((X, protocol_overrides), dim=-1)
+
+        nb_obs_per_chain = patient_index.shape[0]
         prediction_index_expanded = (
             torch.arange(num_chains).repeat_interleave(nb_obs_per_chain),
-            prediction_index[0].repeat(num_chains),
-            prediction_index[1].repeat(num_chains),
-            prediction_index[2].repeat(num_chains),
+            patient_index.repeat(num_chains),
+            timestep_index.repeat(num_chains),
+            task_index.apply_(lambda i: self.task_idx_to_output_idx[i]).repeat(
+                num_chains
+            ),
         )
-        params = X.split(1, dim=-1)
+        # map the "columns" (in fact the last axis which corresponds to the parameters) of X_with_protocol_overrides
+        # to the positions of the corresponding arguments in the signature of the "equations" function
+        params = X_with_protocol_overrides[
+            :, :, :, self.input_tensor_column_index_to_function_parameter_index
+        ].split(1, dim=-1)
         outputs = self.equations(*params)
         y = outputs[prediction_index_expanded].view(num_chains, nb_obs_per_chain)
         pred_var = torch.zeros_like(y)
-
         return y, pred_var
--- vpop_calibration/test/analytical/test_analytical_saem.py
+++ vpop_calibration/test/analytical/test_analytical_saem.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
 import pandas as pd
+from torch.testing import assert_close
 
 from vpop_calibration import *
 
@@ -10,9 +11,8 @@ def test_analytical_saem(np_rng):
         y = lambda1 / (1 + torch.exp(-(t - lambda2) / lambda3))
         return y
 
-    params = ["lambda1", "lambda2", "lambda3"]
     outputs = ["circ"]
-    struct_model = StructuralAnalytical(logistic_growth, params, outputs)
+    struct_model = StructuralAnalytical(logistic_growth, outputs)
 
     patient_df = pd.DataFrame({"id": ["tree-1", "tree-2"]})
     df = patient_df.merge(pd.DataFrame({"output_name": outputs}), how="cross")
@@ -49,3 +49,196 @@ def test_analytical_saem(np_rng):
     plot_weighted_residuals(nlme_model, "iwres")
     plot_weighted_residuals(nlme_model, "pwres")
     plot_weighted_residuals(nlme_model, "npde")
+
+
+def test_analytical_no_protocol():
+    def logistic_growth(lambda1, lambda2, lambda3, t):
+        return lambda1 / (1 + torch.exp(-(t - lambda2) / lambda3))
+
+    struct_model = StructuralAnalytical(
+        logistic_growth,
+        ["circumference"],
+        protocol_design=None,
+    )
+
+    assert struct_model.parameter_names == ["lambda1", "lambda2", "lambda3"]
+    assert struct_model.nb_protocol_overrides == 0
+    assert struct_model.tasks == ["circumference_identity"]
+    assert struct_model.task_idx_to_protocol == {0: "identity"}
+    assert struct_model.task_idx_to_output_idx == {0: 0}
+    num_tasks = len(struct_model.tasks)
+    num_protocol_parameters = 0
+    assert struct_model.task_protocol_tensor.shape == (
+        num_tasks,
+        num_protocol_parameters,
+    )
+
+
+def test_analytical_one_arm_one_override():
+    def logistic_growth(lambda1, lambda2, lambda3, t, sun_power_multiplicator):
+        return lambda1 / (
+            1 + torch.exp(-(sun_power_multiplicator * (t - lambda2) / lambda3))
+        )
+
+    struct_model = StructuralAnalytical(
+        logistic_growth,
+        ["circumference"],
+        protocol_design=pd.DataFrame(
+            data={"protocol_arm": ["Italy"], "sun_power_multiplicator": [1.05]}
+        ),
+    )
+
+    assert struct_model.parameter_names == [
+        "lambda1",
+        "lambda2",
+        "lambda3",
+    ]
+    assert struct_model.nb_protocol_overrides == 1
+    assert struct_model.tasks == ["circumference_Italy"]
+    assert struct_model.task_idx_to_protocol == {0: "Italy"}
+    assert struct_model.task_idx_to_output_idx == {0: 0}
+    num_tasks = len(struct_model.tasks)
+    num_protocol_parameters = 1
+    assert struct_model.task_protocol_tensor.shape == (
+        num_tasks,
+        num_protocol_parameters,
+    )
+
+
+def test_analytical_one_arm_two_overrides():
+    # Here we scrambled the function arguments on purpose, to check
+    # that the logic of self.input_tensor_column_index_to_function_parameter_index
+    # is properly implemented
+    def logistic_growth(
+        lambda3,
+        t,
+        max_circumference_multiplicator,
+        lambda2,
+        lambda1,
+        sun_power_multiplicator,
+    ):
+        return (
+            max_circumference_multiplicator
+            * lambda1
+            / (1 + torch.exp(-(sun_power_multiplicator * (t - lambda2) / lambda3)))
+        )
+
+    struct_model = StructuralAnalytical(
+        logistic_growth,
+        ["circumference"],
+        protocol_design=pd.DataFrame(
+            data={
+                "protocol_arm": ["Italy"],
+                "sun_power_multiplicator": [1.05],
+                "max_circumference_multiplicator": [1.1],
+            }
+        ),
+    )
+
+    assert struct_model.parameter_names == [
+        "lambda3",
+        "lambda2",
+        "lambda1",
+    ]
+
+    # internally we have input_parameters = parameter_names_without_protocol_overrides + ["t"] + protocol_parameters
+    # ["lambda3", "lambda2", "lambda1", "t", "max_circumference_multiplicator", "sun_power_multiplicator"]
+    #      0         1           2       3                    4                          5
+    assert struct_model.input_tensor_column_index_to_function_parameter_index == [
+        0,  # position of "lambda3"
+        3,  # position of "t"
+        4,  # position of "max_circumference_multiplicator"
+        1,  # position of "lambda2"
+        2,  # position of "lambda1"
+        5,  # position of "sun_power_multiplicator"
+    ]
+    assert struct_model.nb_protocol_overrides == 2
+    assert struct_model.tasks == ["circumference_Italy"]
+    assert struct_model.task_idx_to_protocol == {0: "Italy"}
+    assert struct_model.task_idx_to_output_idx == {0: 0}
+    num_tasks = len(struct_model.tasks)
+    num_protocol_parameters = 2
+    assert struct_model.task_protocol_tensor.shape == (
+        num_tasks,
+        num_protocol_parameters,
+    )
+
+
+def test_analytical_two_arms_two_overrides():
+    def logistic_growth(
+        lambda1,
+        lambda2,
+        lambda3,
+        t,
+        sun_power_multiplicator,
+        max_circumference_multiplicator,
+    ):
+        return (
+            max_circumference_multiplicator
+            * lambda1
+            / (1 + torch.exp(-(sun_power_multiplicator * (t - lambda2) / lambda3)))
+        )
+
+    protocol_design = pd.DataFrame(
+        data={
+            "protocol_arm": ["Italy", "Morocco"],
+            "sun_power_multiplicator": [1.05, 1.2],
+            "max_circumference_multiplicator": [1.1, 1.25],
+        }
+    )
+    struct_model = StructuralAnalytical(
+        logistic_growth,
+        ["circumference"],
+        protocol_design=protocol_design,
+    )
+
+    assert struct_model.parameter_names == [
+        "lambda1",
+        "lambda2",
+        "lambda3",
+    ]
+    assert struct_model.nb_protocol_overrides == 2
+    assert struct_model.tasks == ["circumference_Italy", "circumference_Morocco"]
+    assert struct_model.task_idx_to_protocol == {0: "Italy", 1: "Morocco"}
+    assert struct_model.task_idx_to_output_idx == {0: 0, 1: 0}
+    num_tasks = len(struct_model.tasks)
+    num_protocol_parameters = 2
+    assert struct_model.task_protocol_tensor.shape == (
+        num_tasks,
+        num_protocol_parameters,
+    )
+    patient_index = torch.tensor([0, 0, 0, 1, 1, 1])
+    timestep_index = torch.tensor([0, 1, 2, 0, 1, 2])
+    #                           Italy    Morocco
+    task_index = torch.tensor([0, 0, 0, 1, 1, 1])
+    X = torch.tensor(
+        [
+            [
+                [  # lam1 lam2 lam3  t
+                    [333, 101, 350, 118],  # patient 1 at t1
+                    [333, 101, 350, 484],  # patient 1 at t2
+                    [333, 101, 350, 664],  # patient 1 at t3
+                ],
+                [
+                    [483, 100, 360, 118],  # patient 2 at t1
+                    [483, 100, 360, 484],  # patient 2 at t2
+                    [483, 100, 360, 664],  # patient 2 at t3
+                ],
+            ]
+        ]
+    )
+    (num_chains, nb_patients, nb_timesteps, nb_params) = X.shape
+    actual_y, _pred_var = struct_model.simulate(
+        X, (patient_index, timestep_index, task_index), chunks=None
+    )
+    expected_y = logistic_growth(
+        lambda1=torch.tensor([333, 333, 333, 483, 483, 483]),
+        lambda2=torch.tensor([101, 101, 101, 100, 100, 100]),
+        lambda3=torch.tensor([350, 350, 350, 360, 360, 360]),
+        t=torch.tensor([118, 484, 664, 118, 484, 664]),
+        #                                           Italy          Morocco
+        sun_power_multiplicator=torch.tensor([1.05, 1.05, 1.05, 1.2, 1.2, 1.2]),
+        #                                                  Italy          Morocco
+        max_circumference_multiplicator=torch.tensor([1.1, 1.1, 1.1, 1.25, 1.25, 1.25]),
+    ).reshape((num_chains, nb_patients * nb_timesteps))
+    assert_close(actual_y, expected_y)
