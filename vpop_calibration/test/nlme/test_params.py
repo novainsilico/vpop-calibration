1a885626d43ff7556c31f0a35917476ba47fbc64
Paul Lemarre <paul.lemarre@novainsilico.ai>
2026-05-21 12:59:59 +0200

    feat(nlme params): add constraints, adjust corresponding unit test

--- vpop_calibration/test/nlme/test_params.py
+++ vpop_calibration/test/nlme/test_params.py
@@ -1,7 +1,10 @@
 from vpop_calibration.nlme_model.params import MixedEffectParameters
+from vpop_calibration.data.observed import ObsData
 
 import pytest
 import numpy as np
+import pandas as pd
+from pandera.typing import DataFrame
 
 
 @pytest.fixture
@@ -18,17 +21,34 @@ def sample_nlme_params() -> dict:
                 "prior_mean": 10.0,
                 "prior_omega": 0.1,
                 "covariates": {"foo": {"coef_name": "coef_foo_pdu2", "prior": 0.5}},
+                "constraint": {"low": 1.0, "high": 100.0},
             },
         },
         "error_model": {
             "out_1": {"type": "additive", "sigma": 0.1},
             "out_2": {"type": "proportional", "sigma": 0.5},
         },
+        "pdk": ["pdk_1"],
     }
     return input
 
 
-def test_nlme_params(sample_nlme_params):
+@pytest.fixture
+def obs_data(np_rng) -> ObsData:
+    patients = {"id": ["p1", "p2"], "foo": [0.0, 0.0], "pdk_1": [0.0, 0.0]}
+    protocol_arms = ["arm-A", "arm-B"]
+    outputs = ["out_1", "out_2"]
+    time_steps = np.arange(0, 3.0, 1.0)
+    df = pd.DataFrame.from_dict(patients)
+    df = df.merge(pd.DataFrame(protocol_arms, columns=["protocol_arm"]), how="cross")
+    df = df.merge(pd.DataFrame(outputs, columns=["output_name"]), how="cross")
+    df = df.merge(pd.DataFrame(time_steps, columns=["time"]), how="cross")
+    df["value"] = np.abs(np_rng.normal(0, 1, df.shape[0]))
+    data = ObsData(DataFrame(df))
+    return data
+
+
+def test_nlme_params(sample_nlme_params, obs_data):
     nlme_params = MixedEffectParameters.model_validate(sample_nlme_params)
     assert nlme_params.pdu_names == ["pdu_1", "pdu_2"]
     assert nlme_params.mi_names == ["mi_1"]
@@ -38,5 +58,16 @@ def test_nlme_params(sample_nlme_params):
         "pdu_2",
         "coef_foo_pdu2",
     ]
-    assert nlme_params.beta_init == [np.log(10.0), 0.5, np.log(10.0), 0.5]
+    transformed_prior_pdu1 = np.log(10.0)
+    shifted_pdu2 = (10.0 - 1.0) / (100.0 - 1.0)
+    transformed_prior_pdu2 = np.log(shifted_pdu2 / (1 - shifted_pdu2))
+    assert nlme_params.beta_init == [
+        transformed_prior_pdu1,
+        0.5,
+        transformed_prior_pdu2,
+        0.5,
+    ]
     assert nlme_params.covariate_names == ["foo"]
+    assert nlme_params.pdk == ["pdk_1"]
+
+    nlme_params.validate_data(obs_data)
