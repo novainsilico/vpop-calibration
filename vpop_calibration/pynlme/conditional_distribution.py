feat(conditional): fix conditional sampler, add individual logprob plots, add export of concatenated dataframe for future VPC implementation, ocnvert sampler to an interruptable stream

vpop_calibration/pynlme/conditional_distribution.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython.display import display
 import numpy as np
+import uuid
 
 
 from vpop_calibration.pynlme.model import StatisticalModel
@@ -31,8 +32,6 @@ class ConditionalDistributionSampler:
         self.plot_frequency = self.model.config.plot_frequency
 
     def init_samples(self):
-        if smoke_test:
-            nb_samples = 2
 
         if self.live_plot:
             self.build_convergence_plot()
@@ -48,9 +47,12 @@ class ConditionalDistributionSampler:
             step_size=0.1,
             complete_likelihood=init_predictions.predictions.sum(dim=0),
         )
+        init_physical = self.model.convert_gaussian_to_physical(
+            psi=self.current_state.gaussian_params, log_mi=self.model.log_mi
+        )
         init_samples = ConditionalDistribSamples(
             eta_samples=init_etas,
-            physical_params_samples=self.current_state.gaussian_params,
+            physical_params_samples=init_physical,
             predictions=self.current_state.prediction,
             log_prob=self.current_state.log_prob,
         )
@@ -58,24 +60,39 @@ class ConditionalDistributionSampler:
         self.ebe: ConditionalDistribSamples = init_samples
         self.nb_improved_history: list[float] = [0]
         self.mean_improved_history: list[float] = [0]
+        self.indiv_log_prob: np.ndarray = init_samples.log_prob.cpu().numpy()
 
     def run_sampler(self, nb_samples: int = 100):
         self.init_samples()
 
-        for _ in tqdm(range(nb_samples), disable=not self.progress_bar):
+        if smoke_test:
+            nb_samples = 2
+        try:
+            for i in self.sampling_stream(nb_samples):
+                if self.live_plot:
+                    self.update_convergence_plot()
+            plt.close(self.fig)
+        except KeyboardInterrupt:
+            print("Interrupting sampling.")
+            plt.close(self.fig)
+
+    def sampling_stream(self, nb_samples: int):
+        for i in tqdm(range(nb_samples), disable=not self.progress_bar):
             self.current_state = mh_step(
                 self.model, previous_state=self.current_state, learning_rate=0.0
             )
+            new_physical = self.model.convert_gaussian_to_physical(
+                psi=self.current_state.gaussian_params, log_mi=self.model.log_mi
+            )
             new_samples = ConditionalDistribSamples(
                 eta_samples=self.current_state.etas,
-                physical_params_samples=self.current_state.gaussian_params,
+                physical_params_samples=new_physical,
                 predictions=self.current_state.prediction,
                 log_prob=self.current_state.log_prob,
             )
             self.samples.append(new_samples)
             self.update_ebe(new_samples)
-            if self.live_plot:
-                self.update_convergence_plot()
+            yield i
 
     def update_ebe(self, new_samples: ConditionalDistribSamples):
         # Assemble as mask to accept or reject new EBEs
@@ -107,38 +124,47 @@ class ConditionalDistributionSampler:
         self.nb_improved_history.append(nb_improved)
         mean_improved = accept_mask.float().mean().item()
         self.mean_improved_history.append(mean_improved)
+        self.indiv_log_prob = np.concat((self.indiv_log_prob, new_log_prob), axis=0)
 
-    def build_convergence_plot(self, plot_indiv_figsize=(5.0, 3.0)):
+    def build_convergence_plot(self, plot_indiv_figsize=(5.0, 5.0)):
         self.fig, self.axes = plt.subplots(
-            2, 1, figsize=plot_indiv_figsize, sharex=True
+            3, 1, figsize=plot_indiv_figsize, sharex=True
         )
 
         for ax in self.axes:
             ax.grid(True)
 
         self.axes[0].set_title("EBE convergence")
-
         self.axes[0].set_ylabel("Patients improved")
+
         self.axes[1].set_ylabel("Mean LL gain")
-        self.axes[1].set_xlabel("Iteration")
+
+        self.axes[2].set_ylabel("Individual LL")
+        self.axes[2].set_xlabel("Iteration")
 
         (line1_raw,) = self.axes[0].plot([], color="lightgray", linewidth=1)
         (line1_ma,) = self.axes[0].plot([], linewidth=2)
         (line2_raw,) = self.axes[1].plot([], color="lightgray", linewidth=1)
         (line2_ma,) = self.axes[1].plot([], linewidth=2)
+        patient_lines = []
+        for _ in range(self.model.nb_patients):
+            (line,) = self.axes[2].plot([], linewidth=1)
+            patient_lines.append(line)
 
         self.traces = {
             "num_improved": line1_raw,
             "num_improved_ma": line1_ma,
             "mean_gain": line2_raw,
             "mean_gain_ma": line2_ma,
+            "individual": patient_lines,
         }
         if not smoke_test:
             self.handle = display(self.fig, display_id=True)
 
     def update_convergence_plot(self):
 
-        x = np.arange(len(self.nb_improved_history))
+        nb_iters_so_far = len(self.nb_improved_history)
+        x = np.arange(nb_iters_so_far)
 
         mean_gain_ma = moving_average(self.mean_improved_history, window=20)
         num_improved_ma = moving_average(self.nb_improved_history, window=20)
@@ -159,15 +185,57 @@ class ConditionalDistributionSampler:
             x,
             mean_gain_ma,
         )
+        for i in range(self.model.nb_patients):
+            self.traces["individual"][i].set_data(x, self.indiv_log_prob[:, i])
 
         if not smoke_test:
             for ax in self.axes:
                 ax.relim()
                 ax.autoscale_view()
 
-        if hasattr(self, "handle"):
-            if self.handle is not None:
-                self.handle.update(self.fig)
+            if hasattr(self, "handle"):
+                if self.handle is not None:
+                    self.handle.update(self.fig)
+
+    @property
+    def ebe_parameters_df(self) -> pd.DataFrame:
+        theta = self.model.convert_physical_to_thetas_all_patients(
+            self.ebe.physical_params_samples
+        )
+        df = self.model.convert_theta_to_dataframe(theta)
+        return df
+
+    @property
+    def ebe_predictions_df(self) -> pd.DataFrame:
+        df = self.model.data.full_obs.to_pandas(prediction=self.ebe.predictions)
+        return df
+
+    @property
+    def total_samples(self) -> ConditionalDistribSamples:
+        eta = torch.cat([s.eta_samples for s in self.samples], dim=0)
+        physical = torch.cat([s.physical_params_samples for s in self.samples])
+        pred = torch.stack([s.predictions for s in self.samples], dim=0)
+        log_prob = torch.stack([s.log_prob for s in self.samples])
+        out = ConditionalDistribSamples(
+            eta_samples=eta,
+            physical_params_samples=physical,
+            predictions=pred,
+            log_prob=log_prob,
+        )
+        return out
+
+    @property
+    def total_samples_predictions_df(self) -> pd.DataFrame:
+        all_df = []
+        for sample in self.samples:
+            this_sample_df = self.model.data.full_obs.to_pandas(
+                prediction=sample.predictions
+            ).rename(columns={"id": "ref_id"})
+            new_ids = {patient: str(uuid.uuid4()) for patient in self.model.patients}
+            this_sample_df["id"] = this_sample_df["ref_id"].map(new_ids)
+            all_df.append(this_sample_df)
+        total_df = pd.concat(all_df)
+        return total_df
 
 
 def moving_average(x: list[float], window: int = 20) -> np.ndarray: