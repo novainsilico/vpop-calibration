1e4b63b3927cc29e517e6eae5bf076209f390ad1
Paul Lemarre <paul.lemarre@novainsilico.ai>
2026-07-21 11:42:48 +0200

    chore(tests): do not ignore order in DeepDiff, otherwise float comparison is ignored

--- vpop_calibration/test/golden/test_saem_golden.py
+++ vpop_calibration/test/golden/test_saem_golden.py
@@ -93,7 +93,6 @@ def test_saem_golden(sample_nlme_params, obs_data, struct_model, golden, request
         diff = DeepDiff(
             actual,
             expected,
-            ignore_order=True,
             ignore_type_in_groups=[(tuple, list), (float, np.float64)],
             math_epsilon=1e-16,
         )
