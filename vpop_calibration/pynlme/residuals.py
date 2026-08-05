fix: repair ordering in likelihood computation, make test setup fonctional

vpop_calibration/pynlme/residuals.py
@@ -37,23 +37,6 @@ class ResidualErrorEstimates(NamedTuple):
             ),
         )
 
-    @classmethod
-    def uninitialized(cls, nb_outputs: int) -> "ResidualErrorEstimates":
-        return cls(
-            sigma_add=torch.full(
-                (nb_outputs,),
-                -1.0,
-                device=device,
-            ),
-            sigma_prop=torch.full(
-                (nb_outputs,),
-                -1.0,
-                device=device,
-            ),
-            additive_output=torch.ones(nb_outputs, device=device, dtype=torch.bool),
-            proportional_output=torch.ones(nb_outputs, device=device, dtype=torch.bool),
-        )
-
     def assert_initialized(self) -> None:
         if bool(((self.sigma_add < 0.0) | (self.sigma_prop < 0.0)).any()):
             raise RuntimeError(
@@ -195,35 +178,31 @@ def compute_survival_likelihood(
     )
     log_hz_rows_expanded = log_hz_rows.expand(nb_samples, -1)
     cumulative_hz_rows_expanded = cumulative_hz_rows.expand(nb_samples, -1)
-    # Patient indices have no reason to be sorted in the observation data set
-    # Get the ordering index tensor for each required output
-    patient_index_log_hz = torch.argsort(
-        observations.obs_index.id.index_values[log_hz_rows]
-    )
-    # Gather the rows where the prediction contains the log_hazard, and order them by increasing patient index
-    log_hz_predicted = (
-        predictions[log_hz_rows_expanded]
-        .view(nb_samples, nb_patients)
-        .index_select(1, patient_index_log_hz)
-    )
+    # Gather the rows where the prediction contains the log_hazard
+    log_hz_predicted = predictions[log_hz_rows_expanded].view(nb_samples, -1)
 
-    # Same thing for cumulative hazard
-    patient_index_cum_hz = torch.argsort(
-        observations.obs_index.id.index_values[cumulative_hz_rows]
-    )
-    cumulative_hz_predicted = (
-        predictions[cumulative_hz_rows_expanded]
-        .view(nb_samples, nb_patients)
-        .index_select(1, patient_index_cum_hz)
+    cumulative_hz_predicted = predictions[cumulative_hz_rows_expanded].view(
+        nb_samples, -1
     )
 
     event_status = observations.obs_values[log_hz_rows].expand(nb_samples, -1)
 
     # Log likelihood is computed as
     # (event is occurred) * log_hz (event_time) - cumulative_hazard(event_time)
-    ll = event_status * log_hz_predicted - cumulative_hz_predicted
+    ll_surv = event_status * log_hz_predicted - cumulative_hz_predicted
 
-    return ll.view(nb_samples, nb_patients)
+    # This log-likelihood is only computed on patients who have a survival event observation
+
+    patient_index_expanded = observations.obs_index.id.index_values[log_hz_rows].expand(
+        nb_samples, -1
+    )
+
+    ll_per_patient = torch.zeros(
+        (nb_samples, nb_patients), dtype=default_dtype, device=device
+    )
+    ll_per_patient.scatter_add_(1, patient_index_expanded, ll_surv)
+
+    return ll_per_patient
 
 
 def compute_normal_likelihood(

vpop_calibration/test/nlme/test_residuals.py
@@ -338,7 +338,7 @@ def test_residuals_survival():
     event_status_p1 = 1.0
     event_status_p2 = 0.0
     obs_values = torch.tensor(
-        [event_status_p1, event_status_p1, event_status_p2, event_status_p2],
+        [event_status_p2, event_status_p2, event_status_p1, event_status_p1],
         dtype=default_dtype,
     )
 
@@ -373,7 +373,8 @@ def test_joint_likelihood():
     outputs: list = ["log_hz", "cumulative_hz", "out1"]
     event_time_p1 = 2
     event_time_p2 = 3
-    time: list = [event_time_p1, event_time_p2, 0]
+    obs_time_cont = 10
+    time: list = [event_time_p1, event_time_p2, obs_time_cont]
     tasks: list = ["log_hz_arm-1", "cumulative_hz_arm-1", "out1_arm-1"]
 
     # Patients are not ordered
@@ -393,20 +394,13 @@ def test_joint_likelihood():
         index_values=torch.tensor([1, 1, 0, 0, 2]),
         ref_values=time,
         raw_values=pd.Series(
-            [event_time_p2, event_time_p2, event_time_p1, event_time_p1, 0]
+            [event_time_p2, event_time_p2, event_time_p1, event_time_p1, obs_time_cont]
         ),
     )
     protocol_indices = TensorIndexing(
         index_values=torch.tensor([0, 0, 0, 0, 0]),
         ref_values=protocols,
-        raw_values=pd.Series(
-            [
-                "arm_1",
-                "arm_1",
-                "arm_1",
-                "arm_1",
-            ]
-        ),
+        raw_values=pd.Series(["arm_1", "arm_1", "arm_1", "arm_1", "arm_1"]),
     )
     task_indices = TensorIndexing(
         index_values=torch.tensor([0, 1, 0, 1, 2]),
@@ -436,10 +430,10 @@ def test_joint_likelihood():
     obs_continuous_out1 = 10.0
     obs_values = torch.tensor(
         [
-            event_status_p1,
-            event_status_p1,
             event_status_p2,
             event_status_p2,
+            event_status_p1,
+            event_status_p1,
             obs_continuous_out1,
         ],
         dtype=default_dtype,
@@ -488,8 +482,8 @@ def test_joint_likelihood():
     ll_p1 = event_status_p1 * pred_log_hz_p1 - pred_cum_hz_p1
     ll_p2 = event_status_p2 * pred_log_hz_p2 - pred_cum_hz_p2
     ll_out1 = -0.5 * (
-        log(2 * pi * sigma_val**2)
-        + ((pred_continuous_out1 - obs_continuous_out1) ** 2 / sigma_val**2)
+        log(2 * pi * sigma_val)
+        + ((pred_continuous_out1 - obs_continuous_out1) ** 2 / sigma_val)
     )
     expected_ll = torch.tensor(
         [[ll_p1 + ll_out1, ll_p2], [ll_p1 + ll_out1, ll_p2]], dtype=default_dtype