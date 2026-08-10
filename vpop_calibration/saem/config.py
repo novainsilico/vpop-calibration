714600bb383e2369c66ce314ff963f123378bb0b
Jérôme Barsky <jerome.barsky@novainsilico.ai>
2026-08-10 10:02:01 +0200

    adding show_summary as default

--- vpop_calibration/saem/config.py
+++ vpop_calibration/saem/config.py
@@ -30,7 +30,7 @@ class SaemConfigDict(NamedTuple):
 
     # Fim
     covMethod: Literal["sa", "none"] = "sa"
-    nb_iter_fim: int = 50
+    nb_iter_fim: int = 100
     # Output mode selector
     mode: Literal["debug", "cli", "notebook"] = "notebook"
 
--- vpop_calibration/saem/optimizer.py
+++ vpop_calibration/saem/optimizer.py
@@ -174,7 +174,10 @@ class PySaem:
 
             if self.config.live_plot:
                 self.plot.close()
-                delattr(self, "plot")
+
+            if self.config.covMethod == "sa" and hasattr(self, "fim_estimator"):
+                self.fim_estimator.summary()
+
         except KeyboardInterrupt:
             print(f"Interrupted gracefully at iteration {self.scheduler.iteration}.")
 
